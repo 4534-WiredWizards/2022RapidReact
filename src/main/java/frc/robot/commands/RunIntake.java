c4b5815b47ac883c13c02089207dfd83dc404783
Natalie <bubumk10@gmail.com>
2022-02-01 20:21:54 -0500

    changes to intake

--- src/main/java/frc/robot/commands/RunIntake.java
+++ src/main/java/frc/robot/commands/RunIntake.java
@@ -4,18 +4,43 @@
 
 package frc.robot.commands;
 
+import java.util.Map;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.InputDevices;
 import frc.robot.subsystems.Intake;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class RunIntake extends CommandBase {
   /** Creates a new RunIntake. */
   private final Intake m_intake;
+  private double leftIntakeSpeed;
+  private double rightIntakeSpeed;
+  private double centerIntakeSpeed;
 
   public RunIntake(Intake intake) {
     // Use addRequirements() here to declare subsystem dependencies.
     m_intake = intake;
     addRequirements(m_intake);
+
+    Shuffleboard.getTab("Intake")
+    .add("LeftIntakeSpeed", 0)
+    .withWidget(BuiltInWidgets.kNumberSlider)
+    .withProperties(Map.of("min", -1, "max", 1))
+    .getEntry();
+
+    Shuffleboard.getTab("Intake")
+    .add("RightIntakeSpeed", 0)
+    .withWidget(BuiltInWidgets.kNumberSlider)
+    .withProperties(Map.of("min", -1, "max", 1))
+    .getEntry();
+
+    Shuffleboard.getTab("Intake")
+    .add("CenterIntakeSpeed", 0)
+    .withWidget(BuiltInWidgets.kNumberSlider)
+    .withProperties(Map.of("min", -1, "max", 1))
+    .getEntry();
   }
 
   // Called when the command is initially scheduled.
@@ -25,7 +50,10 @@ public class RunIntake extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double intakespeed = 0.0;
+    leftIntakeSpeed = SmartDashboard.getNumber("LeftIntakeSpeed", 0);
+    rightIntakeSpeed = SmartDashboard.getNumber("RightIntakeSpeed", 0);
+    centerIntakeSpeed = SmartDashboard.getNumber("CenterIntakeSpeed", 0);
+
     if (frc.robot.RobotContainer.m_joystick.getRawButton(InputDevices.btn_b)) {
       m_intake.setCenterPiston(!m_intake.getCenterPiston());
     }
@@ -40,41 +68,41 @@ public class RunIntake extends CommandBase {
 
 
     if(frc.robot.RobotContainer.m_joystick.getRawButton(InputDevices.btn_a)) {
-        intakespeed=-0.4;
-        m_intake.setCenterMotor(intakespeed);
+        //intakespeed=-0.4;
+        m_intake.setCenterMotor(centerIntakeSpeed);
     } else {
       if(frc.robot.RobotContainer.m_joystick.getRawButton(InputDevices.btn_y)) {
-        intakespeed=0.4;
-        m_intake.setCenterMotor(intakespeed);
+        //intakespeed=0.4;
+        m_intake.setCenterMotor(centerIntakeSpeed);
       } else {
-        intakespeed=0;
-        m_intake.setCenterMotor(intakespeed);
+        //intakespeed=0;
+        m_intake.setCenterMotor(centerIntakeSpeed);
       }
     }
 
     if(frc.robot.RobotContainer.m_joystick.getRawAxis(InputDevices.btn_leftTrigger) > 0.1 && frc.robot.RobotContainer.m_joystick.getRawButton(InputDevices.btn_a)) {
-      intakespeed=-0.4;
-      m_intake.setLeftMotor(intakespeed);
+      //intakespeed=-0.4;
+      m_intake.setLeftMotor(leftIntakeSpeed);
   } else {
     if(frc.robot.RobotContainer.m_joystick.getRawButton(InputDevices.btn_y)) {
-      intakespeed=0.4;
-      m_intake.setLeftMotor(intakespeed);
+      //intakespeed=0.4;
+      m_intake.setLeftMotor(leftIntakeSpeed);
     } else {
-      intakespeed=0;
-      m_intake.setLeftMotor(intakespeed);
+      //intakespeed=0;
+      m_intake.setLeftMotor(leftIntakeSpeed);
     }
   }
 
   if(frc.robot.RobotContainer.m_joystick.getRawAxis(InputDevices.btn_rightTrigger) > 0.1 && frc.robot.RobotContainer.m_joystick.getRawButton(InputDevices.btn_a)) {
-    intakespeed=-0.4;
-    m_intake.setRightMotor(intakespeed);
+    //intakespeed=-0.4;
+    m_intake.setRightMotor(rightIntakeSpeed);
 } else {
   if(frc.robot.RobotContainer.m_joystick.getRawButton(InputDevices.btn_y)) {
-    intakespeed=0.4;
-    m_intake.setRightMotor(intakespeed);
+    //intakespeed=0.4;
+    m_intake.setRightMotor(rightIntakeSpeed);
   } else {
-    intakespeed=0;
-    m_intake.setRightMotor(intakespeed);
+    //intakespeed=0;
+    m_intake.setRightMotor(rightIntakeSpeed);
   }
 }
 
