95b4a014f518c19e31bcc46805260c5905643aa7
Natalie <bubumk10@gmail.com>
2022-03-09 18:41:10 -0500

    new proxsensor code

--- src/main/java/frc/robot/subsystems/FeederWheel.java
+++ src/main/java/frc/robot/subsystems/FeederWheel.java
@@ -21,7 +21,7 @@ import frc.robot.Constants.SpeedConstants;
 public class FeederWheel extends SubsystemBase {
   /** Creates a new FeederWheel. */
 
-  private final DigitalInput proxSensor = new DigitalInput(0);
+  private final DigitalInput proxSensor = new DigitalInput(5);
   
   private CANSparkMax feedMotor;
   private ShuffleboardTab Tab=Shuffleboard.getTab("FeederWheel");
